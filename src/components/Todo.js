7c6fd200310a6708c17d342bf94d9d2192905f88
Sarka <sarka.rezacova@email.cz>
2019-08-20 18:10:11 +0200

    Add new fields

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -5,12 +5,12 @@ import axios from '../axios';
 import TodoButtons from './TodoButtons';
 
 class Todo extends Component {
-  renderText = () => {
-    const { text } = this.props.todo;
-    if (!text) return null;
+  renderDescription = () => {
+    const { description } = this.props.todo;
+    if (!description) return null;
 
     return (
-      <div className="card-text" dangerouslySetInnerHTML={{ __html: text }} />
+      <div className="card-text" dangerouslySetInnerHTML={{ __html: description }} />
     )
   };
 
@@ -27,11 +27,12 @@ class Todo extends Component {
   };
 
   render() {
-    const { createdAt, title, finished } = this.props.todo;
+    const { createdAt, title, finished, state, year, length, imgUrl } = this.props.todo;
     let classes = 'card';
     if (finished) classes += ' border-success';
 
     return (
+      <>
       <div className="todo mb-2">
         <div className={classes}>
           <div className="card-body">
@@ -41,11 +42,30 @@ class Todo extends Component {
             <h6 className="card-subtitle text-muted mb-2">
               Created at {createdAt}
             </h6>
-            {this.renderText()}
+            {this.renderDescription()}
             <TodoButtons todo={this.props.todo} onFinish={this.handleFinish} onRemove={this.handleRemove} />
           </div>
         </div>
       </div>
+      <div className="card mb-3" style={{maxWidth: 540}}>
+        <div className="row no-gutters">
+          <div className="col-md-4">
+            <img src={imgUrl} className="card-img" alt={`${title} film`} />
+          </div>
+            <div className="col-md-8">
+            <div className="card-body">
+              <h5 className="card-title">{title}</h5>
+              <p className="card-text">{state}</p>
+              <p className="card-text">{year}</p>
+              <p className="card-text">{length}</p>
+              <p className="card-text">{this.renderDescription()}</p>
+              <p className="card-text"><small className="text-muted">Created at {createdAt}</small></p>
+              <TodoButtons todo={this.props.todo} onFinish={this.handleFinish} onRemove={this.handleRemove} />
+            </div>
+          </div>
+        </div>
+      </div>
+      </>
     );
   }
 }
--- src/views/AddTodo.js
+++ src/views/AddTodo.js
@@ -3,32 +3,52 @@ import { withRouter } from "react-router-dom";
 
 class AddTodo extends Component {
   state = {
-    title: "",
-    text: ""
+    imgUrl: '',
+    title: '',
+    description: '',
+    genre: 'drama',
+    state: 'washington',
+    year: '',
+    length: ''
   };
 
   handleSubmit = async event => {
     event.preventDefault();
     await this.props.onAdd(this.state);
     this.setState({
+      imgUrl: '',
       title: '',
-      text: ''
+      description: '',
+      genre: 'drama',
+      state: 'washington',
+      year: '',
+      length: ''
     });
     this.props.history.push("/");
   };
 
   handleChange = event => {
     const { name, value } = event.target;
+    console.log(name, value);
     this.setState({
       [name]: value
     });
   };
 
   render() {
-    const { title, text } = this.state;
+    const { imgUrl, title, description, genre, state, year, length } = this.state;
 
     return (
       <form onSubmit={this.handleSubmit}>
+        <input
+          name="imgUrl"
+          type="text"
+          value={imgUrl}
+          onChange={this.handleChange}
+          className="form-control mb-2"
+          placeholder="Image URL"
+          required
+        />
         <input
           name="title"
           type="text"
@@ -39,13 +59,53 @@ class AddTodo extends Component {
           required
         />
         <textarea
-          name="text"
-          value={text}
+          name="description"
+          value={description}
           onChange={this.handleChange}
           className="form-control mb-2"
-          placeholder="Text"
+          placeholder="Description (html tags allowed)"
         />
-
+        <select
+          name="genre"
+          value={genre}
+          onChange={this.handleChange}
+          className="form-control mb-2"
+          required
+        >
+          <option value="drama">Drama</option>
+          <option value="fantasy">Fantasy</option>
+          <option value="scifi">Scifi</option>
+          <option value="comedy">Comedy</option>
+        </select>
+        <select
+          name="state"
+          value={state}
+          onChange={this.handleChange}
+          className="form-control mb-2"
+          required
+        >
+          <option value="washington">Washington</option>
+          <option value="czechRepublic">Czech republic</option>
+          <option value="malaysia">Malaysia</option>
+        </select>
+        <input
+          name="year"
+          type="number"
+          value={year}
+          onChange={this.handleChange}
+          className="form-control mb-2"
+          placeholder="Year"
+          required
+        />
+        <input
+          name="length"
+          type="number"
+          value={length}
+          onChange={this.handleChange}
+          className="form-control mb-2"
+          placeholder="Length"
+          required
+        />
         <button
           type="submit"
           className="btn btn-outline-success"
