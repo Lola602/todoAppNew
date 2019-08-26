dae643cb1b4118027e94d3dd7e93193f361b0a21
Sarka <sarka.rezacova@email.cz>
2019-08-26 18:58:09 +0200

    Add hard-coded trailer

--- src/components/FilmDetail.js
+++ src/components/FilmDetail.js
@@ -4,7 +4,12 @@ class FilmDetail extends Component {
     render() {
       const { title } = this.props.film;
       return (
-      <p>{title}</p>
+      <>
+        <p>{title}</p>
+        <div class="embed-responsive embed-responsive-16by9">
+          <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" allowfullscreen></iframe>
+        </div>
+      </>
       )
     }
   }
