24863e29b11e0dbd80f5bcc0e10dc16f5db4a2e1
simonka <sim.matejcekova@gmail.com>
2019-08-26 08:59:18 +0200

    ...

--- src/views/FilmList.js
+++ src/views/FilmList.js
@@ -9,45 +9,39 @@ class FilmList extends Component {
   render() {
     const films = this.props.films;
 
-
     return (
         <div>
-       <Masonry>
-        <Link className="link-plus card plus todo mb-2" to="/add">
-          <GoPlus className="plus" />
-          Add new film
-        </Link>
-       </Masonry>
+          <Masonry>
+            <Link className="link-plus card plus todo mb-2" to="/add">
+              <GoPlus className="plus" />
+              Add new film
+            </Link>
+          </Masonry>
 
-      <Masonry className="films">
-        {films.map((filmData) => {
-          const handleShowFilm = () => {
-            filmData.showed === true ? filmData.showed = false : filmData.showed = true;
-            this.props.onEdit(filmData);
-          };
+          <Masonry className="films">
+            {films.map((filmData) => {
+              const handleShowFilm = () => {
+                filmData.showed === true ? filmData.showed = false : filmData.showed = true;
+                this.props.onEdit(filmData);
+              };
 
-          const handleRemoveFilm = () => {
-            this.props.onRemove(filmData);
-          };
+              const handleRemoveFilm = () => {
+                this.props.onRemove(filmData);
+              };
 
-          return (
-              <div>
-
-
-
-              <Film
-              film={filmData}
-              key={filmData.id}
-              onShow={handleShowFilm}
-              onRemove={handleRemoveFilm}
-            />
-              </div>
-
-          );
-        })}
-      </Masonry>
+              return (
+                  <div>
+                    <Film
+                        film={filmData}
+                        key={filmData.id}
+                        onShow={handleShowFilm}
+                        onRemove={handleRemoveFilm}
+                    />
+                  </div>
+              );
+            })}
+          </Masonry>
         </div>
-
     );
   }
 }
